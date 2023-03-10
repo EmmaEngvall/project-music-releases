9ddb6437f9d5cc69e02d327318a6886e9e85ec73
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-03-10 12:43:56 +0100

    Add separator line after header, add wrapper / margin to css, add image container

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -5,23 +5,23 @@ import { Icons } from 'components/Icons';
 
 export const Album = (props) => {
   return (
-    <div className="wrapper">
-      <article className="albumcontainer">
+    <article className="albumcontainer">
+      <div className="imgcontainer">
         <a href={props.albumurl}>
           <img className="album-img" src={props.image} alt="Album Cover" />
           <Icons />
         </a>
-        <a href={props.albumurl}>
-          <div className="albumname">{props.albumname}</div>
-        </a>
-        <div className="artistcontainer">
-          {props.artists.map((artist) => {
-            return (
-              <Artist artist={artist} />
-            )
-          })}
-        </div>
-      </article>
-    </div>
+      </div>
+      <a href={props.albumurl}>
+        <div className="albumname">{props.albumname}</div>
+      </a>
+      <div className="artistcontainer">
+        {props.artists.map((artist) => {
+          return (
+            <Artist artist={artist} />
+          )
+        })}
+      </div>
+    </article>
   )
 }
--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Header.css';
 
+/*
 export const Header = () => {
   return <h1>New albums &amp; singles</h1>;
+}
+*/
+export const Header = () => {
+  return (
+    <div className="headerContainer">
+      <h1 className="Title">New Albums and Singles</h1>
+      <hr />
+    </div>
+  );
 }
